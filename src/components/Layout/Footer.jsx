ec1375bb934a908c2b56a84c3341da7c5c502d6f
ambikabhawsar <ambikabhavsar6@gmail.com>
2024-05-26 00:42:22 +0530

    copyright 2024

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -145,7 +145,7 @@ export default function Footer() {
         <div
           className="text-center p-4"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
-          © 2021 Copyright:
+          © 2024 Copyright:
           <a
             className="text-reset fw-bold"
             href="https://house-cart-master.vercel.app/">
